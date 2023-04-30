0b90751e2b0f32163094573b58c03511b9c3c97f
seulgi <guseul9405@gmail.com>
2023-04-30 18:41:14 +0900

    TS, 테일윈드 설정 및 헤더 푸터

---
+++ src/pages/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='w-full h-12 bg-slate-700'>
+      <p className='text-white font-light text-sm text-center pt-3'>Made by GuSeulgi</p>
+    </div>
+  )
+}
