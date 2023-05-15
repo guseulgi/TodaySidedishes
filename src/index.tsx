6a60ce6f66447422000d552319fbdb85de2d4212
seulgi <guseul9405@gmail.com>
2023-05-15 15:33:08 +0900

    커밋누락

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import {RecoilRoot} from 'recoil';
 
@@ -21,4 +20,4 @@ root.render(
   </BrowserRouter>
 );
 
-reportWebVitals();
+// reportWebVitals();
